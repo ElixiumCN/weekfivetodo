e8824b1a31fe8a3e63bc536660cf46b4d8d87728
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-08-11 16:27:45 +0100

    .
    .

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 // Importing components
 import Form from "./components/Form";
 import TodoList from './components/TodoList';
-
-
 // data does not flow upwards in react
 // states and props flow downwards into components
 // 3:39 hello
@@ -14,15 +12,11 @@ function App() {
   return (
     <div className="App">
       <header>
-        {/* <h1>ElixiumCN Todo List {inputText}</h1> */}
-        {/* reading the value of inputText & printing it in an h1 tag */}
         <h1>ElixiumCN Todo List</h1>
       </header>
       <Form 
         inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-        //
       <TodoList setTodos={setTodos} todos={todos}/> 
-      {/* passing const todos from above down here */}
     </div>
   );
 }
